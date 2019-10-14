a00ce1bbfa8a15aec68213e1d5992619c1ad32ed
sheailun <sheailun@kingsoft.com>
2019-10-14 16:26:40 +0800

    全面审核所有的 plugins 和 presets, 并进行全面更新

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
-# babel-preset-xfe
+# @xfe-team/babel-preset-xfe
 
-> babel preset 定制集成, 目前主要针对日常 react 项目, 支持 IE9+
+> babel preset 定制集成, 目前主要针对日常 react 项目, 支持 IE9+, 其中源码 src/index.ts 进行全面注释说明
 
-## Installation
+## 安装
 
 ```bash
 npm install @xfe-team/babel-preset-xfe --registry=https://registry.npmjs.org/
 ```
 
+## 意图
+
+目前有非常多的 babel-preset, 而当前 babel-preset 主要为了以下目的而创建
+
+1. 可控与稳定, 团队找那个所有的 babel plugin 必须通过测试和审核才会被加入当前 babel preset 中. 如果使用第三方 preset 我们可能因对第三方 preset 不理解而发生底层编译异常且无法第一时间响应修复.
+2. 及时响应更新, 根据团队中成员反馈会及时评估并更新最新 babel plugin, 这样有效提高大家工作效率.
+3. 减少或避免业务开发中对 babel 的复杂配置.
+
+## 参考
+* [babel-preset-umi](https://www.npmjs.com/search?q=babel-preset-umi) - 全面对新功能支持
+* [babel-preset-react-app](https://www.npmjs.com/search?q=babel-preset-react-app) - create-react-app 官方 preset, 虽部分配置符合国内情况, 但经社区考验, 修复大量已知或个人未考量的问题
+
 ## ChangeLog
 
-## 0.0.1 (2018-01-16)
+## 0.0.1 (2019-10-14)
 
-* feat: 默认不启用 set body font size 方案
-* feat: 加入仅对移动端生效的检测
+* feat: init commit
 
 ## 作者
-Ailun She
+She Ailun
 
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,11 @@ export default function (context, opts = {} as any) {
   const {
     useBuiltIns = false,
     loose = false,
-    // We want Create React App to be IE 9 compatible until React itself
-    // no longer works with IE 9
+    // 尽量降级以用于支持 IE9
     targets = {
       ie: 9,
     },
-    // Do not transform modules to CJS. Use for tree shaking
+    // false, 用于支持 tree shaking
     modules = false,
     env = {},
   } = opts;
@@ -21,6 +20,8 @@ export default function (context, opts = {} as any) {
         corejs: false,
         helpers: true,
         absoluteRuntime: dirname(require.resolve('../package')),
+        // TODO: 测试以下开启是否有根本影响, 尝试开启的原因是为了更小的 build
+        useESModules: true
       };
   const exclude = [
     // Exclude transforms that make all code slower
@@ -35,32 +36,187 @@ export default function (context, opts = {} as any) {
   ];
 
   const plugins = [
-    require.resolve('babel-plugin-react-require'),
+    /**
+     * 当前插件主要用于让 import 支持 http 引入形式, 就像 Deno 一样: import zepto from 'FETCH:https://cdn.bootcss.com/zepto/1.0rc1/zepto.min.js';
+     * 详情: https://www.npmjs.com/package/@xfe-team/babel-plugin-fetch-import
+     */
+    require.resolve('@xfe-team/babel-plugin-fetch-import'),
+    /**
+     * 考虑 IDE 支持度, 所有都应该手动加上 react import, 保持统一, 故: 禁用 babel-plugin-react-require
+     */
+    // require.resolve('babel-plugin-react-require'),
+    /**
+     * 用于支持动态 import, 如  import('./haha.js').then(...)
+     * 详情: https://babeljs.io/docs/en/next/babel-plugin-syntax-dynamic-import.html
+     */
     require.resolve('@babel/plugin-syntax-dynamic-import'),
+
+    /**
+     * 用于支持 let n = { x, y, ...z };
+     * 其中, useBuiltIns: true 使用 Object.assign, 反之使用 Babel's extends helper
+     * 详情: https://babeljs.io/docs/en/next/babel-plugin-proposal-object-rest-spread.html
+     */
     [require.resolve('@babel/plugin-proposal-object-rest-spread'), {loose, useBuiltIns}],
+
+    /**
+     * 用于支持一下 example, 其中 catch 可以不写 catch(e)
+     * try {
+     *   throw 0;
+     * } catch {
+     *   doSomethingWhichDoesNotCareAboutTheValueThrown();
+     * }
+     *  详情: https://babeljs.io/docs/en/next/babel-plugin-proposal-optional-catch-binding.html
+     */
     require.resolve('@babel/plugin-proposal-optional-catch-binding'),
+    /**
+     * 用于支持一下 example, 其中 catch 可以不写 catch(e)
+     * async function* agf() {
+     *   await 1;
+     *   yield 2;
+     * }
+     *
+     *  详情: https://babeljs.io/docs/en/next/babel-plugin-proposal-async-generator-functions.html
+     */
     require.resolve('@babel/plugin-proposal-async-generator-functions'),
 
-    // 下面两个的顺序的配置都不能动
+    // 下面两个的顺序的配置都不能动, 先后顺序必须为 @babel/plugin-proposal-decorators, @babel/plugin-proposal-class-properties !!!
+    /**
+     * 用于支持装饰器, 如以下例子
+     * @annotation
+     * class MyClass { }
+     */
     [require.resolve('@babel/plugin-proposal-decorators'), {legacy: true}],
+    /**
+     * 用于支持一下例子:
+     * class Bork {
+     *    //Property initializer syntax
+     *    instanceProperty = "bork";
+     *    boundFunction = () => {
+     *      return this.instanceProperty;
+     * };
+     *
+     * 使用参数 { "loose": true }
+     * var Bork = function Bork() {
+     *   babelHelpers.classCallCheck(this, Bork);
+     *   this.x = 'bar';
+     *   this.y = void 0;
+     * };
+     *
+     * 使用参数 { "loose": false }
+     * var Bork = function Bork() {
+     *   babelHelpers.classCallCheck(this, Bork);
+     *   Object.defineProperty(this, "x", {
+     *     configurable: true,
+     *     enumerable: true,
+     *     writable: true,
+     *     value: 'bar'
+     *   });
+     *   Object.defineProperty(this, "y", {
+     *     configurable: true,
+     *  .....
+     * 详情: https://babeljs.io/docs/en/next/babel-plugin-proposal-class-properties.html
+     */
     [require.resolve('@babel/plugin-proposal-class-properties'), {loose: true}],
-
+    /**
+     * 用于支持: export * as ns from 'mod';
+     * 详情: https://babeljs.io/docs/en/babel-plugin-proposal-export-namespace-from
+     */
     require.resolve('@babel/plugin-proposal-export-namespace-from'),
+    /**
+     * 用于支持: export default from "mod";
+     * 详情: https://github.com/tc39/proposal-export-default-from
+     */
     require.resolve('@babel/plugin-proposal-export-default-from'),
+    /**
+     * 用于支持: var foo = object.foo ?? "default";
+     *
+     * In:
+     * var foo = object.foo ?? "default";
+     *
+     * Out:
+     * var _object$foo;
+     * var foo = (_object$foo = object.foo) !== null && _object$foo !== void 0 ? _object$foo : "default";
+     *
+     * https://babeljs.io/docs/en/next/babel-plugin-proposal-nullish-coalescing-operator.html
+     */
     [require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'), {loose}],
+
+    /**
+     * 用于支持 ?. 语法
+     * const obj = {
+     *   foo: {
+     *     bar: {
+     *       baz() {
+     *         return 42;
+     *       },
+     *     },
+     *   },
+     * };
+     *
+     * const baz = obj?.foo?.bar?.baz(); // 42
+     *
+     * const safe = obj?.qux?.baz(); // undefined
+     * const safe2 = obj?.foo.bar.qux?.(); // undefined
+     *
+     * const willThrow = obj?.foo.bar.qux(); // Error: not a function
+     *
+     * // Top function can be called directly, too.
+     * function test() {
+     *   return 42;
+     * }
+     * test?.(); // 42
+     *
+     * exists?.(); // undefined
+     *
+     * 详情: https://babeljs.io/docs/en/next/babel-plugin-proposal-optional-chaining.html
+     */
     [require.resolve('@babel/plugin-proposal-optional-chaining'), {loose}],
+    /**
+     * 用于支持 pipeline 语法, 如:
+     *
+     * let result = exclaim(capitalize(doubleSay("hello")));
+     * result //=> "Hello, hello!"
+     *
+     * let result = "hello"
+     *   |> doubleSay
+     *   |> capitalize
+     *
+     * result //=> "Hello, hello!"
+     */
     [
       require.resolve('@babel/plugin-proposal-pipeline-operator'),
       {
         proposal: 'minimal',
       },
     ],
+    /**
+     * 用于支持 do 语法, do { .. }
+     * let a = do {
+     * if(x > 10) {
+     *   'big';
+     * } else {
+     *
+     */
     require.resolve('@babel/plugin-proposal-do-expressions'),
+
+    /**
+     * 用于支持 :: 语法
+     * obj::func
+     * // is equivalent to:
+     * func.bind(obj)
+     *
+     * 详情: https://babeljs.io/docs/en/next/babel-plugin-proposal-function-bind.html
+     */
     require.resolve('@babel/plugin-proposal-function-bind'),
+
     require.resolve('babel-plugin-macros'),
     // Necessary to include regardless of the environment because
     // in practice some other transforms (such as object-rest-spread)
     // don't work without it: https://github.com/babel/babel/issues/7215
+    /**
+     * 用于处理 let [a, b, ...rest] = arr;
+     * 详情: https://babeljs.io/docs/en/next/babel-plugin-transform-destructuring.html
+     */
     [
       require('@babel/plugin-transform-destructuring').default,
       {
@@ -81,14 +237,35 @@ export default function (context, opts = {} as any) {
         ],
       },
     ],
-    require.resolve('@babel/plugin-transform-object-assign')
+    /**
+     * 用于 Object.assign(a, b); 解析为:
+     * 由于目前全面使用 Object.assign polyfill, 所以统一禁用用于减少代码 build 大小
+     *
+     * var _extends = ...;
+     * _extends(a, b);
+     *
+     */
+    // require.resolve('@babel/plugin-transform-object-assign')
   ];
 
   if (nodeEnv !== 'test' && transformRuntime) {
+    /**
+     * 在运行期对相关语法进行降级
+     * 详情: https://babeljs.io/docs/en/babel-plugin-transform-runtime
+     */
     plugins.push([require.resolve('@babel/plugin-transform-runtime'), transformRuntime]);
   }
   if (nodeEnv === 'production') {
-    plugins.push(require.resolve('babel-plugin-transform-react-remove-prop-types'));
+    /**
+     * 在生产环境移除所有的 react prop types
+     */
+    plugins.push([
+      require('babel-plugin-transform-react-remove-prop-types').default,
+      {
+        // 直接移除 import PropTypes from 'prop-types' 的引用
+        removeImport: true,
+      },
+    ]);
   }
 
   return {
@@ -105,6 +282,6 @@ export default function (context, opts = {} as any) {
       ],
       require.resolve('@babel/preset-react'),
     ],
-    plugins,
+    plugins
   };
 }
